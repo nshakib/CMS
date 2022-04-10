97bd7ea74bb9725e8c950909ddb0208937c8cf4c
nshakib <nazmus.shakib.cse@gmail.com>
2022-04-10 06:56:21 +0600

    started project COntent Management System - CMS, now working Login system

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table("users")->insert([
+            'name' => "Super Admin",
+            'email' => "super@gmail.test",
+            "email_verified_at" => now(),
+            "password" => Hash::make('superadmin'),
+            "remember_token" => Str::random(10),
+            "created_at" => date("Y-m-d H:i:s"),
+            "updated_at" => date("Y-m-d H:i:s"),
+        ]);
+    }
+}
