d9ee6ba12ab7180f89f850f7d3902ccfe1178d3b
nshakib <nazmus.shakib.cse@gmail.com>
2022-05-21 00:31:47 +0600

    post pagination feature added

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -108,6 +108,11 @@
                 @endforelse
              </ul>
           </div>
+          @if ($posts->hasPages())
+             <div class="card-footer">
+                {{ $posts->links('vendor.pagination.bootstrap-4') }}
+             </div>
+          @endif
        </div>
     </div>
   </div>
